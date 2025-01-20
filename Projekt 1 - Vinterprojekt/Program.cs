0d8bee8d8e18887d3a80e1fe450e8c642f64ed3e
Theodor Freed <theodor.freed@elev.ga.ntig.se>
2025-01-20 09:54:18 +0100

    satte upp strukturen till programmer

--- Projekt 1 - Vinterprojekt/Program.cs
+++ Projekt 1 - Vinterprojekt/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("test");
+﻿using System;
+Random random = new Random();
+
+List<string> potentiella_ord = ["ord", "ordigare", "ordigast"]; // Alla potentiella ord
+List<string> gissade_bokstäver = []; // Alla bokstäver som spelaren har gissat
+
+int liv; // mängden liv som spelaren har (5 från början)
+string korrekt_ord = random.Next(potentiella_ord.Count);
+
+hangman();
+
+static void hangman() {
+    Console.WriteLine("test");
+    Console.ReadLine();
+}
+
+static void avslut() {
+
+}
