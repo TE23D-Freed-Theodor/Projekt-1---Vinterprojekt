add2b2ff8647df3413c0487e4bc6ef1c5da4b4c1
Theodor Freed <theodor.freed@elev.ga.ntig.se>
2025-01-27 09:06:10 +0100

    Nu så skrivs ordet ut i början och om bokstaven inte har blivit gissad så skrivs "_"

--- Projekt 1 - Vinterprojekt/Program.cs
+++ Projekt 1 - Vinterprojekt/Program.cs
@@ -1,32 +1,33 @@
 ﻿using System;
 
-class program
-{
-    static Random random = new Random();
-    static List<string> potentiella_ord = new List<string> { "ord", "ordigare", "ordigast" }; // Alla potentiella ord
-    static List<string> gissade_bokstäver = new List<string>(); // Alla bokstäver som spelaren har gissat
-    int liv; // mängden liv som spelaren har (5 från början)
-    static string korrekt_ord = "";
+Main();
 
-    static void Main() { 
-        // Det är det här som programmet kommer att köra (hangman):
-        Console.InputEncoding = System.Text.Encoding.Unicode;
-        Console.OutputEncoding = System.Text.Encoding.Unicode;
-        hangman();
+static void Main() { 
+    // Skapar variablerna som skickas till metoderna
+    Random random = new Random();
+    List<string> potentiella_ord = new List<string> { "ord", "ordigare", "ordigast" }; // Alla potentiella ord
+    List<string> gissade_bokstäver = new List<string>(); // Alla bokstäver som spelaren har gissat
+    int liv = 5; // mängden liv som spelaren har (5 från början)
+    string korrekt_ord = "";
+
+    // Kör spelet
+    Console.InputEncoding = System.Text.Encoding.Unicode;
+    Console.OutputEncoding = System.Text.Encoding.Unicode;
+    hangman(random, potentiella_ord, gissade_bokstäver, ref korrekt_ord, ref liv);
+}
+
+static void hangman(Random random, List<string> potentiella_ord, List<string> gissade_bokstäver, ref string korrekt_ord, ref int liv) {
+    gissade_bokstäver.Clear(); // Tömmer listan som heter "gissade bokstäver"
+    korrekt_ord = potentiella_ord[random.Next(potentiella_ord.Count())]; // Hittar det korrekta ordet genom att ta ett slumpmässigt tal från potentiella_ord
+    List<string> korrekt_ord_array = korrekt_ord.Select(c => c.ToString()).ToList();
+    
+    for (int x = 0; x < korrekt_ord.Length; x++) {
+        if (gissade_bokstäver.Contains(korrekt_ord_array[x])) {
+            Console.Write(korrekt_ord[x]);
+        }
+        else {
+            Console.Write("_");
+        }
     }
-
-    static void hangman()
-    {
-        gissade_bokstäver.Clear(); // Tömmer listan som heter "gissade bokstäver"
-        korrekt_ord = potentiella_ord[random.Next(potentiella_ord.Count())]; // Hittar det korrekta ordet genom att ta ett slumpmässigt tal från potentiella_ord
-
-        Console.WriteLine(korrekt_ord);
-        Console.ReadLine();
-    }
-
-    static void avslut()
-    {
-
-    }
-
+    Console.ReadLine();
 }
