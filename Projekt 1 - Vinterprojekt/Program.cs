18bba0cab5c448e067ac7a55fb1fbcd978a022aa
Theodor Freed <theodor.freed@elev.ga.ntig.se>
2025-02-10 08:48:06 +0100

    Importerade all form av ascii-art

--- Projekt 1 - Vinterprojekt/Program.cs
+++ Projekt 1 - Vinterprojekt/Program.cs
@@ -1,20 +1,178 @@
 ﻿using System;
 using Raylib_cs;
 
+Main(args);
 
-Main();
-
-static void Main()
+static void Main(string[] args)
 {
+    int stage = 0; // Om spelaren når stage 11 så förlorar den
+
+    List<string> stages = ["", @"                                                                                    
+                                                    
+                       @@@@@@@@@@@@@@@              
+                @@@@@@@@@@@@@@@@@@@@@@@@            
+             @@@@@@@@@@  @       @@@@@@@@@          
+           @@@@@@ @    @@@ @ @ @@   @@@@@@@         
+         @@@@@@   @   @@ @  @@@    @   @@@@@        
+       @@@@@ @  @    @@  @@@@@ @ @ @  @@@@@@@       
+      @@@@   @ @ @@ @@  @ @@ @ @@ @@@ @ @@@@@@      
+     @@@@ @ @ @  @@ @  @ @@   @ @ @@ @@@ @@@@@@     
+    @@@@@    @@ @@@@@   @@   @ @ @@@ @   @@@@@@@    
+   @@@@    @ @ @@@    @@@@  @ @  @  @   @  @@@@@@   
+   @@@   @  @ @@@@ @ @@ @ @  @@  @   @   @@  @@@@   
+  @@@  @ @@@ @@@  @ @@      @@  @@@@@@ @@  @@@@@@   
+  @@@ @ @@@ @@@  @@  @    @@ @  @  @@@@   @  @@@    
+ @@@  @         @         @          @       @@@    
+ @@@                                         @@@    
+", @"                                                    
+                                                    
+                                                    
+              @@@@                                  
+              @@@@                                  
+              @@@@                                  
+              @@@                                   
+              @@@                                   
+              @@@                                   
+              @@@                                   
+              @@@                                   
+              @@@@                                  
+             @@@@                                   
+             @@@@                                   
+             @@@@                                   
+             @@@@                                   
+              @@@                                   
+              @@@                                   
+             @@@@                                   
+             @@@@      @@@@@@@@@@@@@@@              
+             @@@@@@@@@@@@@@@@@@@@@@@@@@@            
+             @@@@@@@@@@  @       @@@@@@@@@          
+           @@@@@@ @    @@@ @ @ @@   @@@@@@@         
+         @@@@@@   @   @@ @  @@@ @  @   @@@@@        
+       @@@@@ @  @    @@  @@@@ @@ @ @  @@@@@@@       
+      @@@@   @ @ @@ @@  @    @ @@ @@@ @ @@@@@@      
+     @@@@ @ @ @  @@ @  @ @@   @ @ @@ @@@ @@@@@@     
+    @@@@@    @@ @@@@@   @@   @ @  @  @   @@@@@@@    
+   @@@@    @ @ @@@    @@ @  @ @  @  @   @  @@@@@@   
+   @@@   @  @ @@@@ @ @@ @ @  @@  @@  @   @@  @@@@   
+  @@@  @ @@@ @@@  @@@@      @@  @@@@@@ @@@ @@@@@@   
+  @@@ @ @@  @@@  @@  @    @@ @  @  @@@@   @  @@@    
+ @@@  @         @         @          @       @@@    
+ @@@                                         @@@    
+                                                    
+                                                    ", @"                                                    
+                                                    
+                           @@@                      
+              @@@@@@@@@@@@@@@@@@@@@@@@@@@           
+              @@@@@@@@@@@@@                         
+              @@@@     @@@                          
+              @@@     @@                            
+              @@@  @@@                              
+              @@@@@@                                
+              @@@@@                                 
+              @@@                                   
+              @@@@                                  
+             @@@@                                   
+             @@@@                                   
+             @@@@                                   
+             @@@@                                   
+              @@@                                   
+              @@@                                   
+             @@@@                                   
+             @@@@      @@@@@@@@@@@@@@@              
+             @@@@@@@@@@@@@@@@@@@@@@@@@@@            
+             @@@@@@@@@@  @       @@@@@@@@@          
+           @@@@@@ @    @@@ @ @ @@   @@@@@@@         
+         @@@@@@   @   @@ @  @@@ @  @   @@@@@        
+       @@@@@ @  @    @@  @@@@ @@ @ @  @@@@@@@       
+      @@@@   @ @ @@ @@  @    @ @@ @@@ @ @@@@@@      
+     @@@@ @ @ @  @@ @  @ @@   @ @ @@ @@@ @@@@@@     
+    @@@@@    @@ @@@@@   @@   @ @  @  @   @@@@@@@    
+   @@@@    @ @ @@@    @@ @  @ @  @  @   @  @@@@@@   
+   @@@   @  @ @@@@ @ @@ @ @  @@  @@  @   @@  @@@@   
+  @@@  @ @@@ @@@  @@@@      @@  @@@@@@ @@@ @@@@@@   
+  @@@ @ @@  @@@  @@  @    @@ @  @  @@@@   @  @@@    
+ @@@  @         @         @          @       @@@    
+ @@@                                         @@@    
+                                                    
+                                                    ", @"                                                    
+                                                    
+                           @@@                      
+              @@@@@@@@@@@@@@@@@@@@@@@@@@@           
+              @@@@@@@@@@@@@          @@@@           
+              @@@@     @@@           @@@@           
+              @@@     @@             @@@@           
+              @@@  @@@               @@@@           
+              @@@@@@                 @@@@           
+              @@@@@                   @             
+              @@@                                   
+              @@@@                                  
+             @@@@                                   
+             @@@@                                   
+             @@@@                                   
+             @@@@                                   
+              @@@                                   
+              @@@                                   
+             @@@@                                   
+             @@@@      @@@@@@@@@@@@@@@              
+             @@@@@@@@@@@@@@@@@@@@@@@@@@@            
+             @@@@@@@@@@  @       @@@@@@@@@          
+           @@@@@@ @    @@@ @ @ @@   @@@@@@@         
+         @@@@@@   @   @@ @  @@@ @  @   @@@@@        
+       @@@@@ @  @    @@  @@@@ @@ @ @  @@@@@@@       
+      @@@@   @ @ @@ @@  @    @ @@ @@@ @ @@@@@@      
+     @@@@ @ @ @  @@ @  @ @@   @ @ @@ @@@ @@@@@@     
+    @@@@@    @@ @@@@@   @@   @ @  @  @   @@@@@@@    
+   @@@@    @ @ @@@    @@ @  @ @  @  @   @  @@@@@@   
+   @@@   @  @ @@@@ @ @@ @ @  @@  @@  @   @@  @@@@   
+  @@@  @ @@@ @@@  @@@@      @@  @@@@@@ @@@ @@@@@@   
+  @@@ @ @@  @@@  @@  @    @@ @  @  @@@@   @  @@@    
+ @@@  @         @         @          @       @@@    
+ @@@                                         @@@    
+                                                    
+                                                    ", @"                                                    
+                                                    
+                           @@@                      
+              @@@@@@@@@@@@@@@@@@@@@@@@@@@           
+              @@@@@@@@@@@@@          @@@@           
+              @@@@     @@@           @@@@           
+              @@@     @@             @@@@           
+              @@@  @@@               @@@@           
+              @@@@@@                 @@@@           
+              @@@@@                @@ @  @          
+              @@@                @         @        
+              @@@@               @    @ @ @@        
+             @@@@                  @@@@@@@          
+             @@@@                @@@  @ @@          
+             @@@@                     @     @@      
+             @@@@                    @@             
+              @@@                 @@    @@          
+              @@@                         @         
+             @@@@                                   
+             @@@@      @@@@@@@@@@@@@@@              
+             @@@@@@@@@@@@@@@@@@@@@@@@@@@            
+             @@@@@@@@@@  @       @@@@@@@@@          
+           @@@@@@ @    @@@ @ @ @@   @@@@@@@         
+         @@@@@@   @   @@ @  @@@ @  @   @@@@@        
+       @@@@@ @  @    @@  @@@@ @@ @ @  @@@@@@@       
+      @@@@   @ @ @@ @@  @    @ @@ @@@ @ @@@@@@      
+     @@@@ @ @ @  @@ @  @ @    @ @ @@ @@@ @@@@@@     
+    @@@@@    @@ @@@@@   @@   @ @  @  @   @@@@@@@    
+   @@@@    @ @ @@@    @@ @@@@@@  @  @   @  @@@@@@   
+   @@@   @  @ @@@@ @ @@ @ @@@@@  @@  @   @@  @@@@   
+  @@@  @ @@@ @@@  @@@@      @@  @@@@@@ @@@ @@@@@@   
+  @@@ @ @@  @@@  @@  @    @@ @  @  @@@@   @  @@@    
+ @@@  @         @         @          @       @@@    
+ @@@                                         @@@    
+                                                    
+                                                    "];
+
+
     Raylib.InitWindow(1600, 900, "test lol");
     Raylib.SetTargetFPS(60);
-    Raylib.BeginDrawing();
-    Raylib.ClearBackground(Color.Black);
-    Raylib.EndDrawing();
 
     // Skapar variablerna som skickas till metoderna
     Random random = new Random();
-    List<string> potentiella_ord = new List<string> { "ord", "ordigare", "ordigast" }; // Alla potentiella ord
+    List<string> potentiella_ord = new List<string> { "orfdjifhdjsohfiodfoisfd", "ordigarfdsnufhdshfdsofdosjifsdfe", "ordigastjfdioshfoidsjhfoisdjfoisdjofjdsoijfidsjsf" }; // Alla potentiella ord
     List<string> gissade_bokstäver = new List<string>(); // Alla bokstäver som spelaren har gissat
     int liv = 5; // mängden liv som spelaren har (5 från början)
     string korrekt_ord = "";
@@ -24,19 +182,19 @@ static void Main()
     Console.InputEncoding = System.Text.Encoding.Unicode;
     Console.OutputEncoding = System.Text.Encoding.Unicode;
 
-    while (!Raylib.WindowShouldClose()) {hangman(gissning, random, potentiella_ord, gissade_bokstäver, korrekt_ord, liv);}
+    gissade_bokstäver.Clear(); // Tömmer listan som heter "gissade bokstäver"
+    korrekt_ord = potentiella_ord[random.Next(potentiella_ord.Count())]; // Hittar det korrekta ordet genom att ta ett slumpmässigt tal från potentiella_ord
+    List<string> korrekt_ord_array = korrekt_ord.Select(c => c.ToString()).ToList();
+
+    while (!Raylib.WindowShouldClose()) {hangman(gissning, random, potentiella_ord, gissade_bokstäver, korrekt_ord, liv, stage, stages);}
 
     Raylib.CloseWindow();
 }
 
-static void hangman(string gissning, Random random, List<string> potentiella_ord, List<string> gissade_bokstäver, string korrekt_ord, int liv)
+static void hangman(string gissning, Random random, List<string> potentiella_ord, List<string> gissade_bokstäver, string korrekt_ord, int liv, int stage, List<string> stages)
 {
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.Black);
-
-    gissade_bokstäver.Clear(); // Tömmer listan som heter "gissade bokstäver"
-    korrekt_ord = potentiella_ord[random.Next(potentiella_ord.Count())]; // Hittar det korrekta ordet genom att ta ett slumpmässigt tal från potentiella_ord
-    List<string> korrekt_ord_array = korrekt_ord.Select(c => c.ToString()).ToList();
     
     string ord_på_skärm = "";
 
@@ -51,7 +209,17 @@ static void hangman(string gissning, Random random, List<string> potentiella_ord
             }
         }
 
-        Raylib.DrawText(ord_på_skärm, 200, 200, 40, Color.White);
+        int ord_på_skärm_bredd = Raylib.MeasureText(ord_på_skärm, 40);
+
+        string instruktion = "Tryck på en bokstav mellan A-Z för att gissa";
+        int instruktion_bredd = Raylib.MeasureText(instruktion, 40);
+
+        int stage_bredd = Raylib.MeasureText(stages[stage], 1);
+
+        int centerX = (Raylib.GetScreenWidth() - ord_på_skärm_bredd)/2;
+        Raylib.DrawText(ord_på_skärm, centerX, 200, 40, Color.White); // Skriver ut ordet fast om en bokstav finns med i gissade_bokstäver men inte är korrekt så skrivs fortfarande ett "_"
+        Raylib.DrawText(instruktion, (Raylib.GetScreenWidth() - instruktion_bredd) / 2, 300, 40, Color.White); 
+        Raylib.DrawText(stages[stage], (Raylib.GetScreenWidth() - stage_bredd) / 2, 670, 1, Color.White);
 
         break;
     }
--- Projekt 1 - Vinterprojekt/obj/Debug/net8.0/Projekt 1 - Vinterprojekt.AssemblyInfo.cs
+++ Projekt 1 - Vinterprojekt/obj/Debug/net8.0/Projekt 1 - Vinterprojekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projekt 1 - Vinterprojekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f01a7f4586dd9dc73b0afca4b2a845bfbfb64f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa6655d8c4dcf97fdfcbe8409670ad81f49d6805")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projekt 1 - Vinterprojekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projekt 1 - Vinterprojekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
